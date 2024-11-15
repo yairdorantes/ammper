07c337ffa876ccaae346fdc5da1397c74716f6c2
yair <yair@pcmaster>
2024-11-15 16:03:56 -0600

    ammper

--- ammper/settings.py
+++ ammper/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
-    "ammper-production.up.railway.app"
+   "https://ammper-production.up.railway.app"
         "http://localhost:5173",
 
 ]
